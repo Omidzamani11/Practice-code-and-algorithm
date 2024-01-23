3914cfc133f7c3f3e4037bcbd7f468dd3ec97db0
OmidZamani <omidzamani1400@gmail.com>
2024-01-23 14:51:24 +0330

    chane2

--- second.py
+++ second.py
@@ -1,11 +1,12 @@
 def ispalindrome(s):
-    a=0
-    b=len(s)-1
-    while a<b:
-        if s[a].lower()!=s[b].lower():
+    a = 0
+    b = len(s) - 1
+    while a < b:  # اصلاح شرط
+        if s[a].lower() != s[b].lower():
             return False
-        a+=1
-        b-=1
-    return False
+        a += 1  # اصلاح افزایش شمارنده
+        b -= 1  # اصلاح کاهش شمارنده
+    return True  # اگر تمام کاراکترها برابر باشند
 
-ispalindrome("radar")    
+
+print(ispalindrome("radar"))
