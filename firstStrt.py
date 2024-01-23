07698d13aea0eac45f4a82ad4c9f646a590ff48e
OmidZamani <omidzamani1400@gmail.com>
2024-01-23 14:13:29 +0330

    Create firstStrt.py

---
+++ firstStrt.py
@@ -0,0 +1,11 @@
+def strStr2(s,x):
+  if x not in s:
+    return -1
+  n = len(x)
+  for i in range(len(s)):
+    print(i)
+    
+    if s[i:i+n] == x :
+      return i
+  return -1
+strStr2 ("omid","id")
